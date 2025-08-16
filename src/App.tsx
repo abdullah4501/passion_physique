7184d6615f957f49befe01d55c4bd4248e11e182
Abdullah Ahmed <abdullah.ah452001@gmail.com>
2025-08-16 22:00:07 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import CheckoutSession from "./pages/CheckoutSession";
 import CheckoutEbook from "./pages/CheckoutEbook";
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const queryClient = new QueryClient();
 
